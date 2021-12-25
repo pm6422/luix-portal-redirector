affa1d972ee692c3fa69e0446f2c1332caf276c4
刘尧 <liuyao@xforceplus.com>
2021-12-26 00:33:31 +0800

    refactor

--- src/main/java/org/infinity/luix/portalredirector/controller/HomeController.java
+++ src/main/java/org/infinity/luix/portalredirector/controller/HomeController.java
@@ -1,18 +1,13 @@
 package org.infinity.luix.portalredirector.controller;
 
-import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @RestController
 public class HomeController {
-
-    @Resource
-    private Environment env;
 
     /**
      * Home page.
