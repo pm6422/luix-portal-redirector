1a4bed0ea187ca7e9f04050eac0d6593ffb98075
刘尧 <liuyao@xforceplus.com>
2022-01-07 20:49:54 +0800

    refactor yml @...@ placeholder

--- src/main/assembly/scripts/stop.sh
+++ src/main/assembly/scripts/stop.sh
@@ -5,7 +5,7 @@ cd `dirname $0`
 # Set variables
 #------------------------------------------------------------------------------------------------------------
 appName="${project.build.finalName}.${project.packaging}"
-serverPort=${app.server.port}
+serverPort=80
 
 #------------------------------------------------------------------------------------------------------------
 # Check the existing process
