88d9f97eca05f00094fcde28da06e7914f740f5f
刘尧 <liuyao@xforceplus.com>
2022-06-24 21:05:02 +0800

    upgrade to spring 2.7

--- src/main/java/org/infinity/luix/portalredirector/controller/HomeController.java
+++ src/main/java/org/infinity/luix/portalredirector/controller/HomeController.java
@@ -1,6 +1,5 @@
 package org.infinity.luix.portalredirector.controller;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,13 +13,11 @@ public class HomeController {
 
     private String portalUrl = "http://pm6422.club:6040";
 
-    @ApiOperation("set luix portal url")
     @PostMapping("/api/portal-url")
     public void setPortalUrl(@RequestParam(value = "url") String url) {
         this.portalUrl = url;
     }
 
-    @ApiOperation("get luix portal url")
     @GetMapping("/api/portal-url")
     public String getPortalUrl() {
         return this.portalUrl;
